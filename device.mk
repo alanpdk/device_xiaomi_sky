565ed5f6684c9297f509c5af671d59eacaaeae4f
AdarshGrewal <adarshgrewal@gmail.com>
2024-08-25 11:51:40 +0000

    sky: Build audioadsprpcd from source
    Change-Id: I19d38c534edf26886bd354548eaee661dd79f793

--- device.mk
+++ device.mk
@@ -79,6 +79,9 @@ PRODUCT_PACKAGES += \
     sound_trigger.primary.parrot
 
 PRODUCT_PACKAGES += \
+    audioadsprpcd
+
+PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
