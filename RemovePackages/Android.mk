33bfe3cc56f488a14694772e4cc881fc8b09f557
Allan DSouza <allan@allan.co.in>
2024-09-25 21:47:32 +0300

    sky: GMS Package name changes for removal

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := arcore RecorderPrebuilt Photos YouTube Maps SafetyHubPrebuilt Drive PrebuiltGmail Velvet
+LOCAL_OVERRIDES_PACKAGES := arcore Photos YouTube Maps Drive PrebuiltGmail DocumentsUI Gmail2
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
